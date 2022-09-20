40e74b654ff000c94d5c0edf095a549dcad4acdc
yujunda20 <yujunda20@mails.ucas.ac.cn>
2022-09-20 15:01:53 +0000

    init(p2): first init

---
+++ Project2_SimpleKernel/include/os/loader.h
@@ -0,0 +1,13 @@
+#ifndef __INCLUDE_LOADER_H__
+#define __INCLUDE_LOADER_H__
+
+#include <type.h>
+
+//uint64_t load_task_img(char *taskname);
+
+//load_task_img for [p1-task3]
+//uint64_t load_task_img(int taskid);
+
+uint64_t load_task_img(char *taskname,int task_num);
+
+#endif
---
+++ Project2_SimpleKernel/init/main.c
@@ -0,0 +1,225 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
+ *            Copyright (C) 2018 Institute of Computing Technology, CAS
+ *               Author : Han Shukai (email : hanshukai@ict.ac.cn)
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
+ *         The kernel's entry, where most of the initialization work is done.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * */
+
+#include <common.h>
+#include <asm.h>
+#include <asm/unistd.h>
+#include <os/loader.h>
+#include <os/irq.h>
+#include <os/sched.h>
+#include <os/lock.h>
+#include <os/kernel.h>
+#include <os/task.h>
+#include <os/string.h>
+#include <os/mm.h>
+#include <os/time.h>
+#include <sys/syscall.h>
+#include <screen.h>
+#include <printk.h>
+#include <assert.h>
+#include <type.h>
+#include <csr.h>
+
+extern void ret_from_exception();
+
+// Task info array
+#define TASK_ADDRESS 0x58000000
+task_info_t tasks[TASK_MAXNUM];
+short task_num;
+
+void handle_app(void);
+
+static void init_jmptab(void)
+{
+    volatile long (*(*jmptab))() = (volatile long (*(*))())KERNEL_JMPTAB_BASE;
+
+    jmptab[CONSOLE_PUTSTR]  = (long (*)())port_write;
+    jmptab[CONSOLE_PUTCHAR] = (long (*)())port_write_ch;
+    jmptab[CONSOLE_GETCHAR] = (long (*)())port_read_ch;
+    jmptab[SD_READ]         = (long (*)())sd_read;
+    jmptab[QEMU_LOGGING]    = (long (*)())qemu_logging;
+    jmptab[SET_TIMER]       = (long (*)())set_timer;
+    jmptab[READ_FDT]        = (long (*)())read_fdt;
+    jmptab[MOVE_CURSOR]     = (long (*)())screen_move_cursor;
+    jmptab[PRINT]           = (long (*)())printk;
+    jmptab[YIELD]           = (long (*)())do_scheduler;
+    jmptab[MUTEX_INIT]      = (long (*)())do_mutex_lock_init;
+    jmptab[MUTEX_ACQ]       = (long (*)())do_mutex_lock_acquire;
+    jmptab[MUTEX_RELEASE]   = (long (*)())do_mutex_lock_release;
+}
+
+static void init_task_info(void)
+{
+    // TODO: [p1-task4] Init 'tasks' array via reading app-info sector
+    // NOTE: You need to get some related arguments from bootblock first
+    bios_sdread(TASK_ADDRESS, 1, 0);
+    task_num=*((short*)(TASK_ADDRESS+512-10));
+    int info_address=*((int*)(TASK_ADDRESS+512-8));
+    int block_id=info_address/512;
+    int offset=info_address%512;
+    int block_num=(info_address+sizeof(task_info_t)*task_num-1)/512+1-block_id;
+    bios_sdread(TASK_ADDRESS, block_num, block_id);
+    for (int i=0;i<task_num;i++)
+    {
+        tasks[i]=*((task_info_t*)(unsigned long)(TASK_ADDRESS+offset));
+        offset+=sizeof(task_info_t);
+    }
+}
+
+static void init_pcb_stack(
+    ptr_t kernel_stack, ptr_t user_stack, ptr_t entry_point,
+    pcb_t *pcb)
+{
+     /* TODO: [p2-task3] initialization of registers on kernel stack
+      * HINT: sp, ra, sepc, sstatus
+      * NOTE: To run the task in user mode, you should set corresponding bits
+      *     of sstatus(SPP, SPIE, etc.).
+      */
+    regs_context_t *pt_regs =
+        (regs_context_t *)(kernel_stack - sizeof(regs_context_t));
+
+
+    /* TODO: [p2-task1] set sp to simulate just returning from switch_to
+     * NOTE: you should prepare a stack, and push some values to
+     * simulate a callee-saved context.
+     */
+    switchto_context_t *pt_switchto =
+        (switchto_context_t *)((ptr_t)pt_regs - sizeof(switchto_context_t));
+
+}
+
+static void init_pcb(void)
+{
+    /* TODO: [p2-task1] load needed tasks and init their corresponding PCB */
+
+
+    /* TODO: [p2-task1] remember to initialize 'current_running' */
+
+}
+
+static void init_syscall(void)
+{
+    // TODO: [p2-task3] initialize system call table.
+}
+
+int main(void)
+{
+    // Init jump table provided by kernel and bios(ΦωΦ)
+    init_jmptab();
+
+    // Init task information (〃'▽'〃)
+    init_task_info();
+
+    // Init Process Control Blocks |•'-'•) ✧
+    init_pcb();
+    printk("> [INIT] PCB initialization succeeded.\n");
+
+    // Read CPU frequency (｡•ᴗ-)_
+    time_base = bios_read_fdt(TIMEBASE);
+
+    // Init lock mechanism o(´^｀)o
+    init_locks();
+    printk("> [INIT] Lock mechanism initialization succeeded.\n");
+
+    // Init interrupt (^_^)
+    init_exception();
+    printk("> [INIT] Interrupt processing initialization succeeded.\n");
+
+    // Init system call table (0_0)
+    init_syscall();
+    printk("> [INIT] System call initialized successfully.\n");
+
+    // Init screen (QAQ)
+    init_screen();
+    printk("> [INIT] SCREEN initialization succeeded.\n");
+
+    // TODO: [p2-task4] Setup timer interrupt and enable all interrupt globally
+    // NOTE: The function of sstatus.sie is different from sie's
+
+    handle_app();
+
+    // Infinite while loop, where CPU stays in a low-power state (QAQQQQQQQQQQQ)
+    while (1)
+    {
+        // If you do non-preemptive scheduling, it's used to surrender control
+        do_scheduler();
+
+        // If you do preemptive scheduling, they're used to enable CSR_SIE and wfi
+        // enable_preempt();
+        // asm volatile("wfi");
+    }
+
+    return 0;
+}
+
+// copying from [p1-task4]
+void handle_app(void)
+{
+    char task_name[NAME_MAXNUM];
+    int input,len;
+    unsigned long current_address;
+    // init vars
+    len=0;
+    for (int i=0;i<NAME_MAXNUM;i++)
+        task_name[i]=0;
+    bios_putstr("Please input task name\n\r");
+    while(1)
+    {
+        input=bios_getchar();
+        if (input==-1) 
+            continue;
+        else if (input=='\r')
+        {
+            if (len==0)
+            {
+                bios_putstr("Please input task name\n\r");
+                continue;
+            }
+            bios_putstr("\n\r");
+            current_address=load_task_img(task_name,task_num);
+            if (current_address==-1)
+                bios_putstr("Invalid task name!\n\r");
+            else 
+                (*(void(*)())current_address)();
+            for (int i=0;i<=len;i++)
+                task_name[i]=0;
+            len=0;
+        }
+        else 
+        {
+            bios_putchar(input);
+            task_name[len]=input;
+            len++;
+            if (len>=NAME_MAXNUM)
+            {
+                bios_putstr("Task name too long!\n\r");
+                for (int i=0;i<=len;i++)
+                    task_name[i]=0;
+                len=0;
+            }
+        }
+    }
+}
