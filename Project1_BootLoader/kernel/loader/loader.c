0b87595366050de8a57304d01a8f8790e96df57e
yujunda20 <yujunda20@mails.ucas.ac.cn>
2022-09-26 07:36:48 +0000

    refactor(p2-task2): remove used code

--- Project1_BootLoader/kernel/loader/loader.c
+++ Project1_BootLoader/kernel/loader/loader.c
@@ -3,24 +3,14 @@
 #include <os/bios.h>
 #include <type.h>
 
-#define BLOCK_NUM       15
-
-//uint64_t load_task_img(int task_id)
-uint64_t load_task_img(char *taskname, int task_num)
+uint64_t load_task_img(char *taskname, short task_num)
 {
     /**
      * TODO:
      * 1. [p1-task3] load task from image via task id, and return its entrypoint
      * 2. [p1-task4] load task via task name, thus the arg should be 'char *taskname'
      */
-    // The code below load task from image via task id for [p1-task3]
-    /*
-    unsigned int current_address=TASK_MEM_BASE+TASK_SIZE*(task_id-1);
-    unsigned int block_id=1+task_id*BLOCK_NUM; 
-    bios_sdread(current_address,BLOCK_NUM,block_id);
-    return (uint64_t)current_address;
-    */
-
+    
     for (int i=0;i<task_num;i++)
     {
         if (strcmp(taskname,tasks[i].task_name)==0)
